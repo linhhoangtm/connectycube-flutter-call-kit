9777ac610df7a6eee1c5b189c7578539a98b967c
linhhoang <htmlinhvn@gmail.com>
2022-03-08 14:31:48 +0700

    fix error

--- android/src/main/kotlin/com/connectycube/flutter/connectycube_flutter_call_kit/ConnectycubeFCMService.kt
+++ android/src/main/kotlin/com/connectycube/flutter/connectycube_flutter_call_kit/ConnectycubeFCMService.kt
@@ -7,30 +7,37 @@ import com.connectycube.flutter.connectycube_flutter_call_kit.utils.ContextHolde
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import org.json.JSONObject
+import android.util.Log
 
 class ConnectycubeFCMService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        super.onMessageReceived(remoteMessage)
-        ContextHolder.applicationContext = applicationContext
-
         val data = remoteMessage.data
-        if (data.containsKey("signal_type")) {
-            when (data["signal_type"]) {
-                "startCall" -> {
-                    processInviteCallEvent(applicationContext, data)
+        Log.d("ConnectycubeFlutterCallKitPlugin", "onMessageReceived: $data");
+        if (data.containsKey("TypeID")  && data["TypeID"] === "6525c531-937a-4533-8408-3f67118c2d13") {
+            //incomingcall
+            ContextHolder.applicationContext = applicationContext
+            if (data.containsKey("signal_type")) {
+                when (data["signal_type"]) {
+                    "startCall" -> {
+                        processInviteCallEvent(applicationContext, data)
+                    }
+
+                    "endCall" -> {
+                        processEndCallEvent(applicationContext, data)
+                    }
+
+                    "rejectCall" -> {
+                        processEndCallEvent(applicationContext, data)
+                    }
                 }
 
-                "endCall" -> {
-                    processEndCallEvent(applicationContext, data)
-                }
-
-                "rejectCall" -> {
-                    processEndCallEvent(applicationContext, data)
-                }
             }
-
+        } else {
+            super.onMessageReceived(remoteMessage)
         }
+        super.onMessageReceived(remoteMessage)
+
     }
 
     private fun processEndCallEvent(applicationContext: Context, data: Map<String, String>) {
